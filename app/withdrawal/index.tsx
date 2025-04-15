68d813c16a93d7643a64d9f53a17a2d1a016bde6
nFd_noFugaziDev <mubaraqmuhammad06@gmail.com>
2025-04-15 14:23:06 +0100

    changed IP

---
+++ app/withdrawal/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const WithdrawalScreen = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+export default WithdrawalScreen
--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-// const API_BASE_URL = 'http://172.20.10.5:3001';
+const API_BASE_URL = 'http://172.20.10.5:3001';
 
-const API_BASE_URL = 'http://192.168.0.116:8082'; // Ensure this is the correct base URL
+// const API_BASE_URL = 'http://192.168.0.116:8082'; // Ensure this is the correct base URL
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
