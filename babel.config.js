b9bdff90ecabdfe7c796696cf0a2036829b54960
HERMESBLACQK <omotosolilhenxy@gmail.com>
2025-11-04 13:17:34 +0100

    removed all nativewind and tailwind refrence from the project

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
+    presets: ["babel-preset-expo"],
     plugins: [
-      // NOTE: react-native-dotenv removed - incompatible with expo-router
-      // Use process.env.VARIABLE_NAME directly instead (Expo has built-in .env support)
-      // Path aliases matching tsconfig.json
       [
         "module-resolver",
         {
@@ -22,16 +16,9 @@ module.exports = function (api) {
             "@components": "./app/components",
             "@types": "./app/types",
           },
-          extensions: [
-            ".js",
-            ".jsx",
-            ".ts",
-            ".tsx",
-            ".json",
-          ],
+          extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
         },
       ],
-      // react-native-reanimated plugin must be listed last
       "react-native-reanimated/plugin",
     ],
   };
